fb9dfbc274220a0441d5e6def4ffaf6e09ca2b62
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-09-17 14:41:59 +0200

    Added BasicOOP

---
+++ Exercises/Person.cs
@@ -0,0 +1,60 @@
+﻿
+namespace BasicOOP
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public Person mother;
+        public Person father;
+        private double length;
+        private double weight;
+
+        public string GetFullName()
+        {
+            return $"{firstName} {lastName}";
+        }
+        public string GetFullName(string titel)
+        {
+            return $"{titel}.{firstName} {lastName}";
+        }
+        public string GetFullNameReversed()
+        {
+            char[] reversed = new char[GetFullName().Length];
+            string fullName = GetFullName();
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                reversed[i] = fullName[fullName.Length - 1 - i];
+            }
+            string reverse = new string(reversed);
+            return reverse;
+        }
+        public string GetSelfAndParents()
+        {
+            string mothersName = (mother == null) ? "okänd" : mother.GetFullName();
+            string fathersName = (father == null) ? "okänd" : father.GetFullName();
+
+            return $"Ditt namn: {GetFullName()}, Mammas namn: {mothersName}, Pappas namn: {fathersName}";
+        }
+        public void SetLength(double length)
+        {
+            this.length = length;
+        }
+        public double GetLength()
+        {
+            return length;
+        }
+        public void SetWeight(double weight)
+        {
+            this.weight = weight;
+        }
+        public double GetWeight()
+        {
+            return weight;
+        }
+        public double GetBMI()
+        {
+            return weight / (length * 2);
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿using BasicOOP;
+//E1
+Console.WriteLine("E1. Skriva ut namn på personer:");
+Person Person1 = new Person() { firstName = "Albert", lastName = "Ask" };
+Person Person2 = new Person() { firstName = "Bilbert", lastName = "Bask" };
+Console.WriteLine($"{Person1.firstName} {Person1.lastName}");
+Console.WriteLine($"{Person2.firstName} {Person2.lastName}");
+Console.WriteLine();
+Console.WriteLine("*****************************************");
+//E2
+Console.WriteLine("E2. Lägg till en metod som returnerar hela namnet:");
+Console.WriteLine(Person1.GetFullName());
+Console.WriteLine(Person2.GetFullName());
+Console.WriteLine();
+Console.WriteLine("*****************************************");
+//E3
+Console.WriteLine("E3. Lägg till en metod som returnerar namnet baklänges:");
+Console.WriteLine(Person1.GetFullNameReversed());
+Console.WriteLine(Person2.GetFullNameReversed());
+Console.WriteLine();
+Console.WriteLine("*****************************************");
+//E4
+Console.WriteLine("E.4 Overload av GetFullName med titel:");
+Console.WriteLine(Person1.GetFullName("Dr"));
+Console.WriteLine(Person2.GetFullName("Mrs"));
+Console.WriteLine();
+Console.WriteLine("*****************************************");
+//E5 + E6
+Console.WriteLine("E5. Lägg till så personer kan ha föräldrar:");
+Console.WriteLine("E6. Skapa ett objekt som representerar dig och dina föräldrar:");
+Person mySelf = new Person { firstName = "Greta", lastName = "Gris" };
+Person father = new Person() { firstName = "Pappa", lastName = "Gris" };
+Person mother = new Person() { firstName = "Mamma", lastName = "Gris" };
+mySelf.mother = mother;
+mySelf.father = father;
+Console.WriteLine(mySelf.GetFullName());
+Console.WriteLine(mySelf.mother.GetFullName());
+Console.WriteLine(mySelf.father.GetFullName());
+Console.WriteLine();
+Console.WriteLine("*******************************************");
+//E7
+Console.WriteLine("E7. Hämta information om dig och dina föräldrar:");
+mySelf.father = null;
+Console.WriteLine(mySelf.GetSelfAndParents());
+Console.WriteLine();
+Console.WriteLine("**************************************************");
+//E8
+Console.WriteLine("E8. Privat fält, med metoder för att sätta värdet:");
+mySelf.SetLength(1.82);
+Console.WriteLine(mySelf.GetLength());
+Console.WriteLine();
+Console.WriteLine("************************************************");
+//E9
+Console.WriteLine("E9. Vikt och BMI:");
+mySelf.SetWeight(90);
+Console.WriteLine(mySelf.GetWeight());
+Console.WriteLine(mySelf.GetBMI());
