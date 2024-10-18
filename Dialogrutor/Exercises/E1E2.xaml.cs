3d88e2128f460df0f0eb55c5c376b6a6f048f113
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-10-18 15:41:28 +0200

    Add Dialogrutor E1, E2 and E3

---
+++ Dialogrutor/Exercises/E1E2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dialogrutor.Exercises
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Application is about to start!");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Application started!");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to quit?", "Closing window", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ Dialogrutor/Exercises/E3.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dialogrutor.Exercises
+{
+    /// <summary>
+    /// Interaction logic for E3.xaml
+    /// </summary>
+    public partial class E3 : Window
+    {
+        public E3()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuClickOpen(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                InitialDirectory = Directory.GetCurrentDirectory(),
+                Filter = "Textfiler (*.txt)|*.txt|Alla filer (*.*)|*.*"
+            };
+            var result = dialog.ShowDialog();
+       
+            if (result == true)
+            {
+                textBox.Text = File.ReadAllText(dialog.FileName);
+            }
+        }
+
+        private void MenuClickExit(object sender, RoutedEventArgs e)
+        {
+            WindowE3.Close();
+        }
+    }
+}
