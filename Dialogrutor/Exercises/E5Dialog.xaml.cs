09a75c1f7e92f97e07d21b623935fd12a23e737f
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-10-20 17:41:16 +0200

    Add Dialogrutor E5

---
+++ Dialogrutor/Exercises/E5Dialog.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dialogrutor.Exercises
+{
+    /// <summary>
+    /// Interaction logic for E5Dialog.xaml
+    /// </summary>
+    public partial class E5Dialog : Window
+    {
+        public SolidColorBrush Color { get; set; }
+        public E5Dialog(SolidColorBrush color)
+        {
+            InitializeComponent();
+            if (color != null)
+            {
+                sliderRed.Value = color.Color.R;
+                sliderGreen.Value = color.Color.G;
+                sliderBlue.Value = color.Color.B;
+                UpdateColorDisplay();
+            }
+        }
+
+        private void colorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateColorDisplay();
+           
+        }
+
+        private void UpdateColorDisplay()
+        {
+            Color color = new Color { A = 255, R = (byte)sliderRed.Value, G = (byte)sliderGreen.Value, B = (byte)sliderBlue.Value };
+            Color = new SolidColorBrush(color);
+            colorRectangel.Fill = Color;
+        }
+
+        private void Button_Click_Ok(object sender, RoutedEventArgs e)
+        {
+            Dialog.DialogResult = true;
+            Close();
+        }
+
+        private void Button_Click_Cancel(object sender, RoutedEventArgs e)
+        {
+            Dialog.DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ Dialogrutor/Exercises/E5Main.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dialogrutor.Exercises
+{
+    /// <summary>
+    /// Interaction logic for E5Main.xaml
+    /// </summary>
+    public partial class E5Main : Window
+    {
+        public SolidColorBrush Color { get; set; }
+        public E5Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        { 
+            E5Dialog dialogWindow = new(Color);  
+           
+            if (dialogWindow.ShowDialog() == true)
+            {
+                Color = dialogWindow.Color;
+            }
+        }
+    }
+}
