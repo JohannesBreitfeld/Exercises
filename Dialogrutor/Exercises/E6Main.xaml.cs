dbfe1a932b28c7fe5273719d7242fdd3d5d7143c
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-10-22 09:46:04 +0200

    Add Dialogrutor E6

---
+++ Dialogrutor/Exercises/E6Main.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dialogrutor.Exercises
+{
+    /// <summary>
+    /// Interaction logic for E6.xaml
+    /// </summary>
+    public partial class E6Main : Window
+    {
+        public SolidColorBrush Color { get; set; }
+        public E6Main()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Sender = sender as Rectangle;
+            E6Dialog dialogWindow = new(Sender.Fill);
+
+            if (dialogWindow.ShowDialog() == true)
+            {
+                Sender.Fill = dialogWindow.Color;
+            }
+        }
+    }
+}
